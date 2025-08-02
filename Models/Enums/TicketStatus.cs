1bd67105b30592dc6050f46b8c99194ec8b1b562
mhadf <mhadf@MHDFAHES>
2025-08-02 16:13:11 +0300

    added 1 new status

--- Models/Enums/TicketStatus.cs
+++ Models/Enums/TicketStatus.cs
@@ -6,7 +6,8 @@
         InProgress = 1,   // Ticket is being worked on
         OnHold = 2,       // Ticket is temporarily paused
         Resolved = 3,     // Issue has been fixed but not yet closed
-        Closed = 4
+        Closed = 4,
+        Escalated=5
     }
 
 
